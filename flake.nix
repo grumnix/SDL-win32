eae89ff225024cd141212ad1616668d3902ec4c5
Ingo Ruhnke <grumbel@gmail.com>
2022-08-22 11:54:28 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+
+    SDL_src.url = "https://www.libsdl.org/release/SDL-1.2.15.tar.gz";
+    SDL_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, tinycmmc, SDL_src }:
+    tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = SDL;
+
+          SDL = pkgs.stdenv.mkDerivation {
+            pname = "SDL";
+            version = "1.2.15";
+
+            src = SDL_src;
+          };
+        };
+      }
+    );
+}
